04e78632e575ae83e265615581571f07d9335b42
Jacques <daxxas158@gmail.com>
2021-04-27 10:28:36 +0200

    Initial commit

---
+++ Assets/MySphere.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySphere : MonoBehaviour
+{
+    /// <summary>
+    /// Description de PublicMySphere
+    /// </summary>
+    public void PublicMySphere()
+    {
+        Debug.Log("cc");
+    }
+    
+    /// <summary>
+    /// Description de PrivateMySphere
+    /// </summary>
+    private void PrivateMySphere()
+    {
+        Debug.Log("cc");
+    }
+}
